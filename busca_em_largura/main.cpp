a33604fcdb6fc22ec4062effdb9e894a8b479338
Gabriel Cesar <gcal1@live.com>
2017-04-07 20:53:50 -0300

    Add busca em largura

---
+++ busca_em_largura/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <list>
+#include <queue>
+#include <limits.h>
+
+#define N 8
+#define DENSIDADE 0.1
+
+using namespace std;
+
+enum enum_cor { branco, preto, cinza };
+enum Vertices {s, w, r, t, x, v, u, y};
+             //0  1  2  3  4  5  6  7
+
+list<int> Adj[N];
+int cor[N]; //Cor
+int d[N]; // distancia
+int pi[N]; //predecessor
+
+void m(int s) {
+    cout << "Vertice visitado: "
+         << s << "\t"
+         << "Pai: "
+         << pi[s] <<  "\t"
+         << "Distancia: "
+         << d[s] << "\n";
+}
+
+void bfs (list<int> Adj[], int s) {
+    for (int i = 0; i < N; i++) {
+        if (i != s) {
+            cor[i] = branco;
+            d[i] = INT_MAX;
+            pi[i] = -1;
+        }
+    }
+    cor[s] = cinza;
+    d[s] = 0;
+    pi[s] = -1;
+
+    queue<int> q;
+    q.push(s);
+    while (!q.empty()){
+        int u = q.front();
+        for (list<int>::iterator it = Adj[u].begin(); it != Adj[u].end(); it++){
+            if (cor[*it] == branco){
+                cor[*it] = cinza;
+                d[*it] = d[u] + 1;
+                pi[*it] = u;
+                q.push(*it);
+
+                m(*it);
+            }
+        }
+        q.pop();
+        cor[u] = preto;
+    }
+}
+
+void grafo_aleat_dir_lst() {
+    // srand(time(NULL));
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            if (((double) rand() / RAND_MAX) < DENSIDADE)
+                Adj[i].push_back(j);
+}
+
+// Grafo não direcionado do exemplo da busca em largura:
+void inicializa_grafo()
+{
+
+// s, w, r, t, x, v, u, y
+ //0  1  2  3  4  5  6  7
+
+   Adj[s].push_back(w);
+   Adj[s].push_back(r);
+   Adj[r].push_back(s);
+   Adj[r].push_back(v);
+   Adj[v].push_back(r);
+   Adj[w].push_back(s);
+   Adj[w].push_back(t);
+   Adj[w].push_back(x);
+   Adj[t].push_back(w);
+   Adj[t].push_back(x);
+   Adj[t].push_back(u);
+   Adj[x].push_back(w);
+   Adj[x].push_back(t);
+   Adj[x].push_back(u);
+   Adj[x].push_back(y);
+   Adj[u].push_back(t);
+   Adj[u].push_back(x);
+   Adj[u].push_back(y);
+   Adj[y].push_back(x);
+   Adj[y].push_back(u);
+
+   // Grafo Bi-direcionado
+}
+
+int main() {
+    //grafo_aleat_dir_lst();
+    inicializa_grafo();
+    bfs(Adj, 0);
+
+
+    return 0;
+}
+
